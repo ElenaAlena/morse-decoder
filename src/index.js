db365af54970c86afabc361a9ef3d8cb1d937973
ElenaAlena <elena.pigaleva@gmail.com>
2020-10-14 23:57:49 +0300

    feat:  implement morce-decoder func

--- src/index.js
+++ src/index.js
@@ -36,9 +36,29 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
+function add0(expr){
+    return expr.padStart(10-(expr.length % 10)+expr.length,0);
+}
 
 function decode(expr) {
-    // write your solution here
+    let words = expr.split("**********");
+  let decodeWords=[];
+    words.forEach(word => {
+        let letters = add0(word).match(/.{1,10}/g);
+      let decodeWord = "";
+        letters.forEach(letter => {
+          let symbols = letter.match(/.{1,2}/g);
+          let result = symbols.map(function(item, index, array) {
+            if(item=="11") return "-";
+            else if(item=="10") return ".";
+            else return "";
+          });
+          if(result.join("").length)
+            decodeWord +=MORSE_TABLE[(result.join(""))];
+        })
+          decodeWords.push(decodeWord);
+    });
+    return decodeWords.join(" ");
 }
 
 module.exports = {
